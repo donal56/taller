9c022b0d33ba4a4833dff8a138f89a7b6431d452
Sergio Daniel Jimenez Zcarias <hmaforsiv@gmail.com>
2019-10-14 18:45:31 -0500

    Update VenVentasController.php

--- controllers/VenVentasController.php
+++ controllers/VenVentasController.php
@@ -380,42 +380,6 @@ class VenVentasController extends Controller
 
     }
 
-     public function actionReport3() 
-    {
-        # $model =  $this->findModel($id);
-
-        $pdf = new Pdf([
-            'format' => [220, 340],
-            'orientation' => Pdf::ORIENT_PORTRAIT, 
-            'destination' => Pdf::DEST_BROWSER, 
-            'marginTop' => '10',
-            'marginHeader' => '10',
-            'marginBottom' => '10',
-            'marginFooter' => '10',
-            'options' => ['title' => 'Orden de servicio'],
-        ]);
-        
-        $mpdf = $pdf->api;
-        $mpdf->autoPageBreak = false;
-
-        //imagenes
-        $mpdf->imageVars['donpolo'] = file_get_contents('img/bluepolo.png');
-        $mpdf->imageVars['logopolo'] = file_get_contents('img/logopolo_letra.png');
-        $mpdf->imageVars['facebook'] = file_get_contents('img/facebook.png');
-        $mpdf->imageVars['whats'] = file_get_contents('img/bluewa.png');
-
-        $mpdf->imageVars['polo'] = file_get_contents('img/logopolo.jpg');
-        
-        $pdf->cssFile = '@app/web/css/pdf4.css';
-        $pdf->content = $this->renderPartial('pdf_format'); 
-
-         return $pdf->render();
-
-
-    }
-
-
-
     protected function findAllProducto($id)
     {
         if (($model = VenProducto::find()->where(['pro_fkventas' => $id])->asArray()->all()) !== null) {
