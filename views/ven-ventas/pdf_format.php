e23d94b0631fa0b776013d9186029d82c531c5c9
Sergio Daniel Jimenez Zcarias <hmaforsiv@gmail.com>
2019-10-14 17:46:28 -0500

    Recibo added

---
+++ views/ven-ventas/pdf_format.php
@@ -0,0 +1,109 @@
+<div >
+
+	<div style="width: 80mm; float: left;">
+	
+		<div style="float:left;  width: 80mm;"><img style="float:left;width: 80mm;" src="var:polo"></div>
+
+		<span class="fblue">No es con espada ni con ejército, sino el poder del espíritu santo</span>
+
+		<div class="fblue bold" style="float:left;">
+			CALLE NiÑOS HEROES No. 165 COL. ATASTA	<br>
+			C.P. 86100 VILLAHERMOSA, CENTRO, TABASCO
+		</div>
+	</div>	
+
+	<div class="border">
+		<div style="font-size: 13px;">
+			<b>POLO TRANSMISIONES AUTOMÁTICAS S. DE R.L. DE C.V.</b>
+		</div>
+		<span class="fblue">R.F.C. PTA0907035Q1</span><br><br>
+		<div class="rounded">
+			<table width="110mm" >
+			  <tr>
+			    <td class="fwhite" style="border-right: 1.5px solid #3369b7;border-bottom: 1.5px solid #3369b7;">FECHA</td>
+			    <td class="fwhite" style="border-bottom: 1.5px solid #3369b7;">RECIBO</td>
+			  </tr>
+			  <tr>
+			  	<td style="font-size:2em;border-right: 1.5px solid #3369b7"> <?= date('y-m-d') ?></td>
+			  	<td style="color:red; font-size:2em;">
+			  		No.
+			  	</td>
+			  </tr> 
+			</table>
+		</div>
+	</div>	
+</div>
+<div class="fullheight rounded">
+		<div style="width: 30mm;" class="div-label">RECIBÍ DE:</div>
+		<div style="width: 150mm;" class="div-underline">&#8203;</div>
+	<br><br>
+		<div style="width: 47mm;" class="div-label">LA CANTIDAD DE:</div>
+		<div style="width: 133mm;" class="div-underline">&#8203;</div>
+	<br><br>
+		<div style="width: 53mm;" class="div-label">POR CONCEPTO DE:</div>
+		<div style="width: 127mm;" class="div-underline">&#8203;</div>
+	<br><br><br>
+	<div class="firmas">
+		<div class="div-label">RECIBIÓ</div>
+		<br><br>
+		<div class="center-line">&#8203;</div>
+		<div class="div-label">NOMBRE Y FIRMA</div>
+	</div>
+	<br>
+</div>
+
+<hr>
+
+<div >
+
+	<div style="width: 80mm; float: left;">
+	
+		<div style="float:left;  width: 80mm;"><img style="float:left;width: 80mm;" src="var:polo"></div>
+
+		<span class="fblue">No es con espada ni con ejército, sino el poder del espíritu santo</span>
+
+		<div class="fblue bold" style="float:left;">
+			CALLE NiÑOS HEROES No. 165 COL. ATASTA	<br>
+			C.P. 86100 VILLAHERMOSA, CENTRO, TABASCO
+		</div>
+	</div>	
+
+	<div class="border">
+		<div style="font-size: 13px;">
+			<b>POLO TRANSMISIONES AUTOMÁTICAS S. DE R.L. DE C.V.</b>
+		</div>
+		<span class="fblue">R.F.C. PTA0907035Q1</span><br><br>
+		<div class="rounded">
+			<table width="110mm" >
+			  <tr>
+			    <td class="fwhite" style="border-right: 1.5px solid #3369b7;border-bottom: 1.5px solid #3369b7;">FECHA</td>
+			    <td class="fwhite" style="border-bottom: 1.5px solid #3369b7;">RECIBO</td>
+			  </tr>
+			  <tr>
+			  	<td style="font-size:2em;border-right: 1.5px solid #3369b7"> <?= date('y-m-d') ?></td>
+			  	<td style="color:red; font-size:2em;">
+			  		No.
+			  	</td>
+			  </tr> 
+			</table>
+		</div>
+	</div>	
+</div>
+<div class="fullheight rounded">
+		<div style="width: 30mm;" class="div-label">RECIBÍ DE:</div>
+		<div style="width: 150mm;" class="div-underline">&#8203;</div>
+	<br><br>
+		<div style="width: 47mm;" class="div-label">LA CANTIDAD DE:</div>
+		<div style="width: 133mm;" class="div-underline">&#8203;</div>
+	<br><br>
+		<div style="width: 53mm;" class="div-label">POR CONCEPTO DE:</div>
+		<div style="width: 127mm;" class="div-underline">&#8203;</div>
+	<br><br><br>
+	<div class="firmas">
+		<div class="div-label">RECIBIÓ</div>
+		<br><br>
+		<div class="center-line">&#8203;</div>
+		<div class="div-label">NOMBRE Y FIRMA</div>
+	</div>
+	<br>
+</div>
