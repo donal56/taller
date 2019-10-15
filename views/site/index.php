df608dba86757cd6192bf8c08ab252ec5c99700e
unknown <donal_56@hotmail.com>
2019-10-15 16:17:24 -0500

    quickfix

--- views/site/index.php
+++ views/site/index.php
@@ -9,12 +9,12 @@ $this->title = 'Polo - Transmisiones autómaticas';
         <tr>
             <td><?= Html::a(Html::img('@web/img/ventas.png', ['alt' => 'Ventas', 'id' => 'ventas']), ['/ven-ventas']) ?></td>
             <td><?= Html::a(Html::img('@web/img/almacen.png', ['alt' => 'Vales de almacén', 'id' => 'almacen']), ['/ven-almacen']) ?></td>
-            <td><?= Html::a(Html::img('@web/img/recibo.png', ['alt' => 'Recibos', 'id' => 'recibos']), ['/ven-recibos']) ?></td>
+            <td><?= Html::a(Html::img('@web/img/recibo.png', ['alt' => 'Recibos', 'id' => 'recibo']), ['/ven-recibo']) ?></td>
         </tr>
         <tr class= 'labels'>
             <td><?= Html::a(Html::label('Ventas', 'ventas'), ['/ven-ventas']) ?></td>
             <td><?= Html::a(Html::label('Vales de almacén', 'almacen'), ['/ven-almacen'])  ?></td>
-            <td><?= Html::a(Html::label('Recibos', 'recibos'), ['/ven-recibos'])  ?></td>
+            <td><?= Html::a(Html::label('Recibos', 'recibo'), ['/ven-recibo'])  ?></td>
         </tr>
     </table>
 </div>
