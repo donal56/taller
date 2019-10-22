2edad7b1bf74e0b15fee93a3342568ac24afb843
Liz Soveranez <elizabeth050698@gmail.com>
2019-10-21 19:08:03 -0500

    PDF4

---
+++ views/ven-orden/body.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<!--Logo-->
+<div style= "position: absolute; top: 25mm; left: 15mm">
+	<img src="var:donpolo" id= "polo" >
+</div>
+<div id="cuadroFolio" class= "bordered"></div>
+
+	<div class="borde">
+		<!-- *************************** header ************************ -->
+		<div class="fondoAzul">
+			<br>
+			<!-- Letras "POLO"-->
+			<div style= "top: 5mm;">
+				<img src="var:logopolo" style= "width: 140mm">	
+				<hr class="azul">
+			</div>
+			<b>No es con espada ni con ejército, sino con el poder del espíritu santo</b>
+			<br>
+			<b>Niños Héroes No. 165 Col. Atasta, Villahermosa, Centro, Tabasco.</b>
+			<br>
+			<b class= "px-14">Tels. 354 86 83 y 354 02 47</b> &nbsp;&nbsp;&nbsp;
+			<img src="var:whats" class= "icon">
+			<b class= "px-14">9933 86 74 17</b>
+			<br>
+			<b class= "px-10"><span class="condensed">Email: polotransmisionesautomaticas@hotmail.com</span></b>&nbsp;
+			<img src="var:facebook" class= "icon">
+			<b class= "px-10">polotransmisionesautomaticas</b>
+			<br><br>
+		</div>	
+		<div id="barraAzul">
+			<span><b class="blanco px-18">ORDEN DE SERVICIO</b></span>
+		</div>
+
+		<!-- *************************** body ************************ -->
+		<div style="width: 100%; height: 20px; float: center;">
+			<div style="width: 15mm ; margin: 0" class="div-label">NOMBRE:</div>
+			<div style="width: 158mm;" class="div-underline">&#8203;</div>
+			<div style="width: 19mm ;" class="div-label">DIRECCIÓN :</div>
+			<div style="width: 153mm;" class="div-underline">&#8203;</div>
+			<div style="width: 13mm ;" class="div-label">C.P.:</div>
+			<div style="width: 18mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">TELEFONO:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">IFE:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">MODELO:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">TIPO:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">MARCA:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">PLACAS:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">FECHA DE INGRESO:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">HORA:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">FECHA DE ENTREGA:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">NO.DE SERIE:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">COLOR:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+			<div style="width: 18mm ;" class="div-label">KM.RECORRIDOS:</div>
+			<div style="width: 27mm ;" class="div-underline">&#8203;</div>
+		</div>
+	</div>
+</body>
+</html>
