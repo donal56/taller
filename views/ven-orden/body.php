ca1bf5ad139fd16c1605f5f06a1a5461059d05b7
Liz Soveranez <elizabeth050698@gmail.com>
2019-10-24 15:53:38 -0500

    pdf

--- views/ven-orden/body.php
+++ views/ven-orden/body.php
@@ -3,17 +3,21 @@
 
 <body>
 <!--Logo-->
-<div style= "position: absolute; top: 25mm; left: 15mm">
+<div style= "position: absolute; top: 16mm; left: 15mm">
 	<img src="var:donpolo" id= "polo" >
 </div>
 <!-- Imagen auto -->
-<div style= "position: absolute; top: 105mm; left: 70mm">
+<div style= "position: absolute; top: 90.5mm; left: 70mm">
 	<img width="50%" src="var:auto" id= "auto" >
 </div>
-
 <!-- linea del tanque -->
 <div class="linea">
 	<img class="unmedio" src="var:linea"  id= "linea" >
+</div>
+<!-- tanque de gasolina -->
+<div class="tanque">
+	<img src="var:fondoTan" id="fondoTanque">
+	<div class="px-12 azul bold">TANQUE DE GASOLINA</div>
 </div>
 <!-- Cuadro del folio -->
 <div id="cuadroFolio" class= "bordered"></div>
@@ -21,29 +25,27 @@
 	<div class="borde">
 		<!-- *************************** header ************************ -->
 		<div class="fondoAzul">
-			<br>
 			<!-- Letras "POLO"-->
-			<div style= "top: 5mm;">
-				<img src="var:logopolo" style= "width: 140mm">	
+			<div>
+				<img src="var:logopolo" id="letraPolo" style= "width: 120mm;">	
 				<hr class="azul">
 			</div>
 			<b>No es con espada ni con ejército, sino con el poder del espíritu santo</b>
 			<br>
 			<b>Niños Héroes No. 165 Col. Atasta, Villahermosa, Centro, Tabasco.</b>
 			<br>
-			<b class= "px-14">Tels. 354 86 83 y 354 02 47</b> &nbsp;&nbsp;&nbsp;
+			<b class= "px-12">Tels. 354 86 83 y 354 02 47</b> &nbsp;&nbsp;&nbsp;
 			<img src="var:whats" class= "icon">
 			<b class= "px-14">9933 86 74 17</b>
 			<br>
-			<b class= "px-10"><span class="condensed">Email: polotransmisionesautomaticas@hotmail.com</span></b>&nbsp;
+			<b class= "px-8"><span class="condensed">Email: polotransmisionesautomaticas@hotmail.com</span></b>&nbsp;
 			<img src="var:facebook" class= "icon">
-			<b class= "px-10">polotransmisionesautomaticas</b>
-			<br><br>
+			<b class= "px-8">polotransmisionesautomaticas</b>
+			<br>
 		</div>	
 		<div id="barraAzul">
-			<span><b class="blanco px-18">ORDEN DE SERVICIO</b></span>
+			<span><b class="blanco px-16">ORDEN DE SERVICIO</b></span>
 		</div>
-
 		<!-- *************************** body ****************************** -->
 		<!--   ********************** 1era Parte ***********************     -->
 		<div class="panel">
@@ -62,7 +64,6 @@
 			
 			<div style="width: 6mm  ;" class="div-label">IFE:</div>
 			<div style="width: 46.3mm ;" class="div-underline">&#8203;</div>
-			
 			<!-- linea4 -->
 			<div style="width: 14mm ;" class="div-label">MODELO:</div>
 			<div style="width: 30mm ;" class="div-underline">&#8203;</div>
@@ -75,7 +76,6 @@
 			
 			<div style="width: 13mm ;" class="div-label">PLACAS:</div>
 			<div style="width: 30mm ;" class="div-underline">&#8203;</div>
-			
 			<!-- linea5 -->
 			<div style="width: 31mm ;" class="div-label">FECHA DE INGRESO:</div>
 			<div style="width: 40mm ;" class="div-underline">&#8203;</div>
@@ -85,7 +85,6 @@
 			
 			<div style="width: 18mm ;" class="div-label">F/ENTREGA:</div>
 			<div style="width: 41.3mm ;" class="div-underline">&#8203;</div>
-
 			<!-- linea6 -->
 			<div style="width: 21mm ;" class="div-label">NO.DE SERIE:</div>
 			<div style="width: 35mm ;" class="div-underline">&#8203;</div>
@@ -130,10 +129,60 @@
 		<div style="width: 30mm ;" class="div-label">OBSERVACIONES:</div>
 		<div style="width: 145mm;" class="div-underline">&#8203;</div>
 
-		<!-- tanque de gasolina -->
-		<div>
-			<img class ="tanque" src="var:fondoTan" id= "fondoTanque" >
+		<!-- titulos2 -->
+		<div style="width: 120mm; height: 5mm; float: right;">
+			<div style="width: 51mm; float:left;" class="div-label2 borde">EXTERIORES</div>
+			<div style="width: 50mm; float:right;" class="div-label2 borde">INTERIORES</div>
 		</div>
+
+		<!-- tablas 2 -->
+		<div style="width: 120mm; height: 5mm; float: right;">
+			<div style="width: 50mm;float:left;">
+				<table class="tabla" border="1">
+					<tr>
+						<th></th>
+						<th>Si</th>	
+						<th>No</th>
+					</tr>
+				</table>
+			</div>
+			<div style="width: 50mm; float:right;">
+				<table class="tabla" border="1">
+					<tr>
+						<th></th>
+						<th>Si</th>	
+						<th>No</th>
+					</tr>
+				</table>
+			</div>
+		</div>
+
+		<br><br><br><br><br><br>
+
+		<!-- problemas del vehiculo -->
+		<div class="borde" style="width: 80mm; height: 50mm; float: left;">
+			<div class="px-14 azul bold" style="text-align: left;">PROBLEMAS DEL VEHICULO</div>
+			<div style="width: 70mm ;" class="div-underline">&#8203;</div>
+		</div>
+		<!-- diagnostico tecnico -->
+		<div class="borde" style="width: 80mm; height: 50mm; float: right;">
+			<div class="px-14 azul bold" style="text-align: left;">DIAGNOSTICO TECNICO</div>
+		</div>
+	</div>
+	<!-- *************************** footer ************************ -->
+	<div class="px-16 azul bold">NOTAS IMPORTANTES</div>
+	<div style="text-align: justify;">1.- EL CLIENTE ESTÁ DE ACUERDO Y AUTORIZA QUE SU UNIDAD SEA PROBADA Y CHECADA FUERA DE NUESTRAS INTSTALACIONES EN UN RADIO DE DISTANCIA NO MAYOR A 50 KILÓMETROS A LA REDONDA PARA GARANTIZAR EL BUEN DESEMPEÑO DE LA MISMAPARA LO CUAL EL CLIENTE FIRMA DE CONFORMIDAD.</div>
+	<div style="text-align: justify;">
+	2.- DURANTE EL PERIODO DE PRUEBA LA RESPONSABILIDAD DE ACCIDENTES O DAÑOS QUE SUFRA LA UNIDAD DURANTE EL PERIODO DE PRUEBA POR MOTIVOS AJENOS A NUESTRA RESPONSABILIDAD TÉCNICA SERA BAJO LA COBERTURA PRIMERAMENTE POR EL SEGURO DEL CLIENTE Y EN CASO DE CARECER DE DICHO SEGURO, EL CLIENTE PAGARÁ EL DEDUCIBLE QUE SE DERIVE EL PAGO DE DAÑOS QUE NUESTRO SEGURO LIQUIDE.
+	</div>
+	<div style="text-align: justify;">
+	3.-EN PARTES ELECTRÓNICAS NO HAY GARANTÍAS.
+	</div>
+	<div style="text-align: justify;">
+	4.- NO NOS HACEMOS RESPONSABLES POR OBJETOS DE VALOR NO REPORTADOS EN LA ADMINISTRACIÓN
+	</div>
+	<div style="text-align: justify;">
+	5.- DESPUÉS DE 24 HRS. QUE HAYA RECIBIDO EL CLIENTELA CONFIRMACIÓN DE TERMINADA SU UNIDAD Y ESTA NO ES RECOGIDA PAGARÁ $50 PESOS DE PENSIÓN DIARIA Y SERÁ RESPONSABLE DE LOS DAÑOS QUE ÉSTA SUF´RA DURANTE LA ESTANCIA.
 	</div>
 </body>
 </html>
