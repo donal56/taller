2e9079ef9d477e02582d12fdfdcbdc7f42646a9d
donal56 <donal_56@hotmail.com>
2019-11-11 14:26:38 -0600

    bd

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=taller',
+    'username' => 'root',
+    'password' => 'pass',
+    'charset' => 'utf8',
+    //'tablePrefix' => 'cur_',
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
